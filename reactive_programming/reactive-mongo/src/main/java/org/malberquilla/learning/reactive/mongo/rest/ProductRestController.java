67da8d11eea728176d9fe91c865c1a78f4afacdc
Alberquilla Puerta, Mario <malberquillap@indra.es>
2023-10-05 10:02:12 +0200

    Delete mapping

--- reactive_programming/reactive-mongo/src/main/java/org/malberquilla/learning/reactive/mongo/rest/ProductRestController.java
+++ reactive_programming/reactive-mongo/src/main/java/org/malberquilla/learning/reactive/mongo/rest/ProductRestController.java
@@ -1,16 +1,20 @@
 package org.malberquilla.learning.reactive.mongo.rest;
 
 import org.malberquilla.learning.reactive.mongo.dto.ProductDto;
+import org.malberquilla.learning.reactive.mongo.exception.ProductNotFoundException;
 import org.malberquilla.learning.reactive.mongo.mapper.UtilProduct;
 import org.malberquilla.learning.reactive.mongo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -32,6 +36,20 @@ public class ProductRestController {
     public Mono<ResponseEntity<ProductDto>> addProduct(@Valid @RequestBody ProductDto product) {
         return this.service.save(UtilProduct.dtoToDomain(product))
             .map(p -> ResponseEntity.ok(UtilProduct.domainToDto(p)));
+    }
+
+    @DeleteMapping("/product/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteProductById(@PathVariable("id") String id) {
+        return service.existsById(id)
+            .flatMap(exists -> {
+                if (Boolean.TRUE.equals(exists)) {
+                    return service.deleteById(id);
+                } else {
+                    return Mono.error(new ProductNotFoundException("Producto no encontrado"));
+                }
+            });
+
     }
 
     @GetMapping("/product")
